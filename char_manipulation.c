93baa8307d4dcbd816069d925438f97ae96a7ae5
Borja Velasco Aguado <borja.v.a@icloud.com>
2022-09-27 19:27:20 +0200

    added atoi

--- char_manipulation.c
+++ char_manipulation.c
@@ -6,18 +6,18 @@
 /*   By: bvelasco <bvelasco@student.42madrid>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/09/12 15:00:37 by bvelasco          #+#    #+#             */
-/*   Updated: 2022/09/20 19:48:14 by bvelasco         ###   ########.fr       */
+/*   Updated: 2022/09/27 16:23:49 by bvelasco         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-int ft_toupper(int c)
+int	ft_toupper(int c)
 {
 	if (c >= 'a' && c <= 'z')
 		return (c - 32);
 	return (c);
 }
 
-int ft_tolower(int c)
+int	ft_tolower(int c)
 {
 	if (c >= 'A' && c <= 'Z')
 		return (c + 32);
