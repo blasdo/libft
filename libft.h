e3fd8e6e0e788c324ec3a21ecf23dd945ded82dc
Borja <borja@MacBook-Air-de-Borja.local>
2022-10-04 14:41:57 +0200

    added ft_strnstr.c but is not done

--- libft.h
+++ libft.h
@@ -41,4 +41,5 @@ void			*ft_memcpy(void *dst, const void *src, size_t n);
 void			*ft_memmove(void *dst, const void *src, size_t len);
 char			*ft_strchr(const char *s, int c);
 char			*ft_strrchr(const char *s, int c);
+char			*ft_strnstr(const char *haystack, const char *needle, size_t len);
 #	endif
