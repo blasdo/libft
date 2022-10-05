c781ffa12d117a2a3d86468119fa086efb213117
Borja Velasco <borja.v.a@outlook.es>
2022-10-05 18:14:51 +0200

    resolver 1 problem

--- ft_strnstr.c
+++ ft_strnstr.c
@@ -6,7 +6,7 @@
 /*   By: bvelasco <bvelasco@student.42madrid.com>   +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/10/05 14:18:44 by bvelasco          #+#    #+#             */
-/*   Updated: 2022/10/05 17:15:56 by bvelasco         ###   ########.fr       */
+/*   Updated: 2022/10/05 15:37:19 by bvelasco         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,18 +17,23 @@ char	*ft_strnstr(const char *haystack, const char *needle, size_t len)
 	size_t	i;
 	size_t	j;
 
+	if (len + 1 == 0)
+		return (0);
 	i = 0;
 	j = 0;
 	if (ft_strlen(needle) == 0 || needle == 0)
 		return((char *) haystack);
 	while( i < len && *haystack != 0)
 	{
-		if (*haystack == *needle)
+		if (*haystack == needle[j])
 		{
-			int j = 0;
-			while (*(haystack + j) == needle[j] && needle[j] != 0)
-				
+			if(needle[j + 1] == 0)
+				return ((char *) haystack - j);
+			j++;
 		}
+		else
+			j = 0;
+		i++;
 		haystack++;
 	}
 	return (0);
