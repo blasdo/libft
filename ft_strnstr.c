f421338a91dc2ebc622b8d00f8b715eb4ff5676d
Borja Velasco <borja.v.a@outlook.es>
2022-10-06 18:42:27 +0200

    change of computer

--- ft_strnstr.c
+++ ft_strnstr.c
@@ -6,7 +6,7 @@
 /*   By: bvelasco <bvelasco@student.42madrid.com>   +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/10/05 14:18:44 by bvelasco          #+#    #+#             */
-/*   Updated: 2022/10/06 14:41:00 by bvelasco         ###   ########.fr       */
+/*   Updated: 2022/10/06 18:35:50 by bvelasco         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -23,7 +23,7 @@ char	*ft_strnstr(const char *haystack, const char *needle, size_t len)
 	j = 0;
 	if (ft_strlen(needle) == 0 || needle == 0)
 		return ((char *) haystack);
-	while (i < len && *haystack != 0)
+	while (i < len && haystack[i] != 0)
 	{
 		j = 0;
 		while (i + j < len && haystack[i + j] == needle[j] && haystack[i + j] != 0)
