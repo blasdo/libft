bb4bcdee03b98151f4a402a970a21d5080ada94e
Borja Velasco Aguado <borja.v.a@icloud.com>
2022-10-31 13:56:58 +0100

    franccinette passed

--- ft_strlcat.c
+++ ft_strlcat.c
@@ -18,7 +18,7 @@ size_t	ft_strlcat(char *dst, const char *src, size_t dstsize)
 	size_t	j;
 	size_t	dst_original_size;
 
-	if (dstsize == 0)
+	if (!dstsize && !dst)
 		return (0);
 	i = ft_strlen(dst);
 	dst_original_size = i;
