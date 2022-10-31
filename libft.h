3a636a49d65a77534cda95e779c205dfce386725
Borja Velasco <borja.v.a@outlook.es>
2022-10-31 07:23:54 +0100

    all methods created, now need testing

--- libft.h
+++ libft.h
@@ -6,13 +6,13 @@
 /*   By: bvelasco <bvelasco@student.42madrid>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/09/12 14:54:00 by bvelasco          #+#    #+#             */
-/*   Updated: 2022/10/30 18:26:41 by bvelasco         ###   ########.fr       */
+/*   Updated: 2022/10/31 07:08:15 by bvelasco         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef LIBFT_H
 # define LIBFT_H
-# include <stddef.h>
+# include <unistd.h>
 # include <stdlib.h>
 # ifndef INT_MAX
 #  define INT_MAX 2147483647
@@ -62,4 +62,10 @@ char	*ft_strjoin(char const *s1, char const *s2);
 char	*ft_strtrim(char const *s1, char const *set);
 char	**ft_split(char const *s, char c);
 char	*ft_itoa(int n);
+char	*ft_strmapi(char const *s, char (*f)(unsigned int, char));
+void	ft_striteri(char *s, void (*f)(unsigned int, char*));
+void	ft_putchar_fd(char c, int fd);
+void	ft_putstr_fd(char *s, int fd);
+void	ft_putendl_fd(char *s, int fd);
+void	ft_putnbr_fd(int n, int fd);
 #endif
