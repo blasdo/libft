0afbbfa1a1ea48aa6e58af28c6def08b12c9e475
Borja Velasco Aguado <borja.v.a@icloud.com>
2022-11-03 14:28:48 +0100

    strlcat don't work

--- ft_strlcat.c
+++ ft_strlcat.c
@@ -6,22 +6,17 @@
 /*   By: bvelasco <bvelasco@student.42madrid.com>   +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/10/07 19:06:00 by bvelasco          #+#    #+#             */
-/*   Updated: 2022/11/03 10:09:01 by bvelasco         ###   ########.fr       */
+/*   Updated: 2022/11/03 14:20:35 by bvelasco         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
-
 #include "libft.h"
 
 size_t	ft_strlcat(char *dst, const char *src, size_t dstsize)
 {
-	int i;
-	int o_dstsize;
+	size_t o_dstsize;
+	size_t size_of_cat;
 
 	o_dstsize = ft_strlen(dst);
-	i = o_dstsize;
-
-	while (i < (dstsize - 1) && *src != 0)
-	{
-		
-	}
+	size_of_cat = ft_strlcpy(dst + o_dstsize, src, dstsize - ft_strlen(dst));
+	return (ft_strlen(src) - size_of_cat);
 }
--- libft.h
+++ libft.h
@@ -6,7 +6,7 @@
 /*   By: bvelasco <bvelasco@student.42madrid>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/09/12 14:54:00 by bvelasco          #+#    #+#             */
-/*   Updated: 2022/11/02 02:00:41 by bvelasco         ###   ########.fr       */
+/*   Updated: 2022/11/03 14:02:13 by bvelasco         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -30,12 +30,12 @@ typedef struct s_list
 	struct s_list	*next;
 }				t_list;
 
-//list methods
 t_list	*ft_lstnew(void *content);
 void	ft_lstadd_front(t_list **lst, t_list *new);
 int		ft_lstsize(t_list *lst);
 t_list	*ft_lstlast(t_list *lst);
 void	ft_lstadd_back(t_list **lst, t_list *new);
+void	ft_lstdelone(t_list *lst, void (*del)(void*));
 void	ft_lstclear(t_list **lst, void (*del)(void*));
 void	ft_lstiter(t_list *lst, void (*f)(void*));
 t_list	*ft_lstmap(t_list *lst, void *(*f)(void *), void (*del)(void *));
@@ -76,5 +76,4 @@ void	ft_putchar_fd(char c, int fd);
 void	ft_putstr_fd(char *s, int fd);
 void	ft_putendl_fd(char *s, int fd);
 void	ft_putnbr_fd(int n, int fd);
-void	ft_lstdelone(t_list *lst, void (*del)(void*));
 #endif
