eef1e60c5b9f26355e7e02d5f6460ecc98e82ba2
Borja Velasco <bvelasco@42madrid.com>
2023-01-14 20:15:00 +0100

    ft_itoa_base working

--- ft_ltoa_base.c
+++ ft_ltoa_base.c
@@ -1,12 +1,12 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   ft_itoa_base.c                                     :+:      :+:    :+:   */
+/*   ft_ltoa_base.c                                     :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: bvelasco <bvelasco@student.42madrid.com>   +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/01/14 16:47:41 by bvelasco          #+#    #+#             */
-/*   Updated: 2023/01/14 17:01:29 by bvelasco         ###   ########.fr       */
+/*   Updated: 2023/01/14 20:13:56 by bvelasco         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -31,5 +31,27 @@ size_t ft_numlen_base(long lnbr, size_t base_len)
 
 char	*ft_ltoa_base(long lnbr, char *base)
 {
+	int		base_len;
+	char	*ret;
+	int		index;
 
+	base_len = ft_strlen(base);
+	index = ft_numlen_base(lnbr, base_len);
+	ret = malloc(index + 1); 
+	if (!ret)
+		return (0);
+	if (lnbr < 0)
+		ret[0] = '-';
+	if (lnbr == 0)
+		ret[0] = '0';
+	ret[index--] = 0;
+	while (lnbr)
+	{
+		if (lnbr > 0)
+			ret[index--] = base[lnbr%base_len];
+		if (lnbr < 0)
+			ret[index--] = base[lnbr%base_len];
+		lnbr /= base_len;
+	}
+	return (ret);
 }
