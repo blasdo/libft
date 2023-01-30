027003f856b836e760cb2ff405cf52cd22756a23
bvelasco <oe4r91t1y@mozmail.com>
2023-01-30 22:10:09 +0100

    solved compatibility problems

--- libft.h
+++ libft.h
@@ -82,7 +82,7 @@ void	ft_putchar_fd(char c, int fd);
 int		ft_putstr_fd(char *s, int fd);
 void	ft_putendl_fd(char *s, int fd);
 void	ft_putnbr_fd(int n, int fd);
-void	ft_putnbr_base_fd(int nbr, char *base, int fd);
+int		ft_putlnbr_base_fd(int nbr, char *base, int fd);
 // Search Char or String in Memory or Null terminated Strings Functions
 void	*ft_memchr(const void *s, int c, size_t n);
 char	*ft_strchr(const char *s, int c);
