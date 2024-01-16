90af7da3922017e556453965f38133733cdad565
Borja Velasco <bvelasco@student.42madrid.com>
2024-01-16 18:16:46 +0100

    solved possible segfault in ft_strncmp

--- srcs/ft_strncmp.c
+++ srcs/ft_strncmp.c
@@ -6,7 +6,7 @@
 /*   By: bvelasco <bvelasco@student.42madrid>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/10/01 13:45:22 by bvelasco          #+#    #+#             */
-/*   Updated: 2023/09/24 01:37:20 by bvelasco         ###   ########.fr       */
+/*   Updated: 2024/01/16 18:15:59 by bvelasco         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,6 +17,8 @@ int	ft_strncmp(const char *s1, const char *s2, size_t n)
 	size_t	i;
 
 	i = 0;
+	if (!s1 || !s1 || !n)
+		return (0):
 	while ((s1[i] != 0 || s2[i] != 0) && i < n)
 	{
 		if (s1[i] != s2[i])
