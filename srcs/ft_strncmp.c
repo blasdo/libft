c9166757383ad24253a1666bfe288ca2263ae8fe
blasdo <72580092+blasdo@users.noreply.github.com>
2024-01-21 16:43:16 +0100

    solved a type fail

--- srcs/ft_strncmp.c
+++ srcs/ft_strncmp.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   ft_strncmp.c                                       :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: bvelasco <bvelasco@student.42madrid>       +#+  +:+       +#+        */
+/*   By: borja <borja@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/10/01 13:45:22 by bvelasco          #+#    #+#             */
-/*   Updated: 2024/01/16 18:15:59 by bvelasco         ###   ########.fr       */
+/*   Updated: 2024/01/21 16:42:53 by borja            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,7 +18,7 @@ int	ft_strncmp(const char *s1, const char *s2, size_t n)
 
 	i = 0;
 	if (!s1 || !s1 || !n)
-		return (0):
+		return (0);
 	while ((s1[i] != 0 || s2[i] != 0) && i < n)
 	{
 		if (s1[i] != s2[i])
