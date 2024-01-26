28476b1f317ed2189f19167fcf5878cbc7feaa08
Borja Velasco <bvelasco@student.42madrid.com>
2024-01-26 16:35:09 +0100

    unified style of new ft_lst*_type functions (in file)

--- srcs/ft_lstnew_bonus.c
+++ srcs/ft_lstnew_bonus.c
@@ -6,23 +6,11 @@
 /*   By: bvelasco <bvelasco@student.42madrid.com>   +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/11/01 13:02:20 by bvelasco          #+#    #+#             */
-/*   Updated: 2024/01/26 16:31:43 by bvelasco         ###   ########.fr       */
+/*   Updated: 2024/01/26 16:34:33 by bvelasco         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <libft.h>
-
-t_list	*ft_lstnew(void *content)
-{
-	t_list	*new;
-
-	new = malloc(sizeof(t_list));
-	if (!new)
-		return (0);
-	new -> content.ptr = content;
-	new -> next = 0;
-	return (new);
-}
 
 t_list	*ft_lstnew_type(t_type type, t_content content)
 {
@@ -34,5 +22,18 @@ t_list	*ft_lstnew_type(t_type type, t_content content)
 	new->type = type;
 	new->content = content;
 	new -> next = NULL;
+	return (new);
+}
+// deprecated
+
+t_list	*ft_lstnew(void *content)
+{
+	t_list	*new;
+
+	new = malloc(sizeof(t_list));
+	if (!new)
+		return (0);
+	new -> content.ptr = content;
+	new -> next = 0;
 	return (new);
 }
